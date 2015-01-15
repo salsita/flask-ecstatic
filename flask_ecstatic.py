912cb18dd4118ad986b6675b0779a2ab836a5806
Jan Killian <jan.killian@gmail.com>
2015-01-15 11:28:48 +0100

    fix: fix serving index file from subdirectories

--- flask_ecstatic.py
+++ flask_ecstatic.py
@@ -13,11 +13,11 @@ __version__ = '0.1.2'
 
 import os
 
-def add(app, url = None, path = None, endpoint=None, decorate=None, index='index.html', send_from_directory=None):
+def add(app, url = None, path = None, endpoint=None, decorate=None, index='index.html', flask=None, **options):
     """Adds static files endpoint with optional directory index."""
 
-    if not send_from_directory:
-        from flask import send_from_directory
+    if not flask:
+        import flask
 
     url = url or app.static_url_path or ''
     path = os.path.abspath(path or app.static_folder or '.')
@@ -26,22 +26,28 @@ def add(app, url = None, path = None, endpoint=None, decorate=None, index='index
     endpoints = {}
 
     if path == app.static_folder:
-        if url != app.static_url_path:
-            raise ValueError('Files in `{}` path are automatically served on `{}` URL by Flask.'
-                ' Use different path for serving them at `{}` URL'.format(path, app.static_url_path, url))
-    else:
-         @app.route(url + '/<path:filename>', endpoint = endpoint)
-         @decorate
-         def static_files(filename):
-             return send_from_directory(path, filename)
+        raise ValueError('Files in `{}` path are already automatically served on `{}` URL by Flask.'
+            ' Set Flask app static_folder to None, if you want to serve them using Flask Ecstatic at `{}` URL'
+            .format(path, app.static_url_path, url))
 
-         endpoints[endpoint] = static_files
+    @app.route(url + '/<path:filename>', endpoint = endpoint)
+    @decorate
+    def static_files(filename):
+        if index:
+            filename = flask.safe_join(path, filename)
+            if os.path.isdir(filename):
+                filename = os.path.join(filename, index)
+            return flask.send_file(filename, **options)
+        else:
+            return flask.send_from_directory(path, filename, **options)
+
+    endpoints[endpoint] = static_files
 
     if index:
          @app.route(url + '/', endpoint = endpoint + '_index')
          @decorate
          def static_index():
-             return send_from_directory(path, index)
+             return flask.send_from_directory(path, index, **options)
 
          endpoints[endpoint + '_index'] = static_index
 
@@ -49,7 +55,7 @@ def add(app, url = None, path = None, endpoint=None, decorate=None, index='index
              @app.route(url, endpoint = endpoint + '_index_bare')
              @decorate
              def static_index_bare():
-                 return send_from_directory(path, index)
+                 return flask.send_from_directory(path, index, **options)
 
              endpoints[endpoint + '_index_bare'] = static_index_bare
 
